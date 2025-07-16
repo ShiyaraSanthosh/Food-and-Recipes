f47da7e5306bcd87cc1fa800006b3f8bc7856fc2
ShiyaraSanthosh <shiyarasanthosh3@gmail.com>
2025-07-17 01:38:53 +0530

    food

--- src/Components/SpecialDishes.jsx
+++ src/Components/SpecialDishes.jsx
@@ -7,7 +7,7 @@ const SpecialDishes = (props) => {
             return(
             <li className='mt-[40px] '>
                
-                <img className = 'max-w-[80%]'src= {menuItem.strMealThumb} />
+                <img className = 'max-w-[80%] rounded overflow-hidden shadow-lg'src= {menuItem.strMealThumb} />
                  <h1 className=' text-[18px]'>{menuItem.strMeal}</h1>
                 
             </li>
@@ -16,7 +16,7 @@ const SpecialDishes = (props) => {
         }
     });
     return (
-        <div >
+        <div>
             <h1 className='m-auto max-w-[200px] text-[20px] font-extrabold text-shadow-lg/30 mt-[50px]'>Our Special Dishes</h1>
             <p className=' max-w-[80%] m-auto p-[20px] ml-[15%] text-[17px]'>Indian cuisine boasts a wide array of dishes, with some considered special due to their unique preparation, rich flavors, or association with special occasions.</p>
             <ul className='grid grid-cols-4 gap-10 m-auto p-[20px] ml-[5%]'>
